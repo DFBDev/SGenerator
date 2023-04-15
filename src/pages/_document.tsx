53e71d1758dc59d86f1c66670aab2cdfd3c6a6bb
Dementexo <browndon962@gmail.com>
2023-04-15 12:25:08 -0500

    potentially fixed font issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
@@ -7,6 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto Mono" />
       </body>
     </Html>
   )
